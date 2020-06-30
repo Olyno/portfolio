8bd4f63b0001555fdd4fb02b0b4ac012c42a3efc
Olyno <alexlew95@gmail.com>
2020-07-01 01:29:55 +0200

    :art: Make projects dynamic

--- src/pages/Projects.svelte
+++ src/pages/Projects.svelte
@@ -1,8 +1,43 @@
 <script>
 
-    import { projects } from '../data';
+    import axios from 'axios';
 
     import ProjectCard from '../components/ProjectCard.svelte';
+
+    let projects = [];
+
+    const languageOrder = [
+        'typescript',
+        'javascript',
+        'html',
+        'java',
+        'dart',
+        'php'
+    ].reverse();
+    
+    const forgotProjects = [
+        "coq-francais",
+        "repro-rollup-issue",
+        "javascript_courses",
+        "datsocialmedia",
+        "portfolio"
+    ]
+
+    axios.get('https://api.github.com/users/Olyno/repos')
+        .then(({ data: repositories }) => {
+            const filteredRepositories = repositories
+                .filter(repository => repository.language !== null && !forgotProjects.includes(repository.name))
+                .sort((a, b) => {
+                    const languageA = a.language.toLowerCase();
+                    const languageB = b.language.toLowerCase();
+                    if (languageOrder.includes(languageA) && languageOrder.includes(languageB)) {
+                        return languageOrder.indexOf(languageB) - languageOrder.indexOf(languageA);
+                    } else {
+                        return languageOrder.includes(languageB) ? 1 : -1;
+                    }
+                })
+            projects = filteredRepositories;
+        })
 
 </script>
 
