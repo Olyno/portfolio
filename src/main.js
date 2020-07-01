9d1988c03e7de655359b5f72d9c84e8dd4c90305
Olyno <alexlew95@gmail.com>
2020-07-01 14:24:27 +0200

    :zap: Make animations running once

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import 'aos/dist/aos.css';
 import 'animate.css/animate.min.css';
 import 'bulma/css/bulma.min.css';
 
-AOS.init();
+AOS.init({
+	once: true
+});
 
 export default new App({
 	target: document.body
