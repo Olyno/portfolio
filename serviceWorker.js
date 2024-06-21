1282ff273fe8fb29c1673a1bc5962d289e8e5ae7
Olyno <Olyno@users.noreply.github.com>
2024-06-21 22:44:52 +0000

    Deploy to GitHub pages

---
+++ serviceWorker.js
@@ -0,0 +1,35 @@
+const cacheName = 'pwa-conf-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './utils.js',
+  './build',
+  './images'
+];
+
+async function networkFirst(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cachedResponse = await cache.match(req);
+    return cachedResponse;
+  }
+}
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cachedResponse = await cache.match(req);
+  return cachedResponse || networkFirst(req);
+}
+
+self.addEventListener('install', async event => {
+  caches.open(cacheName)
+    .then(cache => cache.addAll(staticAssets));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(cacheFirst(event.request));
+});
